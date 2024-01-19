bf538087ccbeee470d5076e4337b52186a600a8b
Daymens Code <Daymenscode@gmail.com>
2024-01-19 06:03:21 -0500

    added PRIMARY KEY to departments table so foreign key in roles table would identify it

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,25 @@ CREATE DATABASE  business_db;
 
 USE business_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT,
   FOREIGN KEY (department_id)
-  REFERENCES department(id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
 
 );
 
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,

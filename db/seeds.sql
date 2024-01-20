e4d296b9c1dc77b573b7d76973c5d6c792c69ec5
Daymens Code <Daymenscode@gmail.com>
2024-01-19 22:06:34 -0500

    fixed syntax error with roles table seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 USE business_db
 
-/* need seed for department */
+
  INSERT INTO departments (id, name)
  VALUES (01, 'HR'),
  (02, 'TechSupport'),
@@ -11,30 +11,31 @@ USE business_db
 
   
 
-/* need seed for role */
+
 INSERT INTO roles (title, salary, department_id)
 VALUES ('Relations Manager', 80000.00, 01),
-VALUES ('Security', 32000.00, 01),
-VALUES ('Onboard Facilitator', 45000.00, 01),
-VALUES ('Support Manager', 85000.00, 02),
-VALUES ('Front-End Support', 45000.00, 02),
-VALUES ('Back-End Support', 75000.00, 02),
-VALUES ('Head Of Finance', 100000.00, 03),
-VALUES ('Payroll Facilitator', 65000.00, 03),
-VALUES ('Financial Advisor', 45000.00, 03),
-VALUES ('Development Manager', 185000.00, 04),
-VALUES ('Data Structure Specialist', 95000.00, 04),
-VALUES ('Data Security', 10500.00, 04),
-VALUES ('Implementation Dev', 100000.00, 04),
-VALUES ('Web Maintanance Technician', 95000.00, 04),
-VALUES ('Marketing Director', 95000.00, 05),
-VALUES ('Business Translator', 75000.00, 05),
-VALUES ('Leads&Adds Facilitator', 65000.00, 05),
-VALUES ('Facilities Manager', 65000.00, 06),
-VALUES ('Santitation Technician', 35000.00, 06),
-VALUES ('Contracted Cleaner', 45000.00, 06);
+ ('Security', 32000.00, 01),
+ ('Onboard Facilitator', 45000.00, 01),
+ ('Support Manager', 85000.00, 02),
+ ('Front-End Support', 45000.00, 02),
+ ('Back-End Support', 75000.00, 02),
+ ('Head Of Finance', 100000.00, 03),
+ ('Payroll Facilitator', 65000.00, 03),
+ ('Financial Advisor', 45000.00, 03),
+ ('Development Manager', 185000.00, 04),
+ ('Data Structure Specialist', 95000.00, 04),
+ ('Data Security', 10500.00, 04),
+ ('Implementation Dev', 100000.00, 04),
+ ('Web Maintanance Technician', 95000.00, 04),
+ ('Marketing Director', 95000.00, 05),
+ ('Business Translator', 75000.00, 05),
+ ('Leads&Adds Facilitator', 65000.00, 05),
+ ('Facilities Manager', 65000.00, 06),
+ ('Santitation Technician', 35000.00, 06),
+ ('Contracted Cleaner', 45000.00, 06);
 
 
 
 
 /* need seeds for employee */
+/* INSERT INTO employees (first_name, last_name, role_id) */

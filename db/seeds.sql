a8489391c33281df6922f6669f8e7775ecec6242
Daymens Code <Daymenscode@gmail.com>
2024-01-20 11:39:33 -0500

    figurin out how to work out the manager_id portion of employee table and seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,31 +12,33 @@ USE business_db
   
 
 
-INSERT INTO roles (id, title, salary, department_id)
-VALUES (01, 'Relations Manager', 80000.00, 01),
- ('Security', 32000.00, 01),
- ('Onboard Facilitator', 45000.00, 01),
- ('Support Manager', 85000.00, 02),
- ('Front-End Support', 45000.00, 02),
- ('Back-End Support', 75000.00, 02),
- ('Head Of Finance', 100000.00, 03),
- ('Payroll Facilitator', 65000.00, 03),
- ('Financial Advisor', 45000.00, 03),
- ('Development Manager', 185000.00, 04),
- ('Data Structure Specialist', 95000.00, 04),
- ('Data Security', 10500.00, 04),
- ('Implementation Dev', 100000.00, 04),
- ('Web Maintanance Technician', 95000.00, 04),
- ('Marketing Director', 95000.00, 05),
- ('Business Translator', 75000.00, 05),
- ('Leads&Adds Facilitator', 65000.00, 05),
- ('Facilities Manager', 65000.00, 06),
- ('Santitation Technician', 35000.00, 06),
- ('Contracted Cleaner', 85000.00, 06);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Relations Manager', 80000.00, 1),
+ ('Security', 32000.00, 1), 
+ ('Onboard Facilitator', 45000.00, 1),
+ ('Support Manager', 85000.00, 2),
+ ('Front-End Support', 45000.00, 2),
+ ('Back-End Support', 75000.00, 2),
+ ('Head Of Finance', 100000.00, 3),
+ ('Payroll Facilitator', 65000.00, 3),
+ ('Financial Advisor', 45000.00, 3),
+ ('Development Manager', 185000.00, 4),
+ ('Data Structure Specialist', 95000.00, 4),
+ ('Data Security', 10500.00, 4),
+ ('Implementation Dev', 100000.00, 4),
+ ('Web Maintanance Technician', 95000.00, 4),
+ ('Marketing Director', 95000.00, 5),
+ ('Business Translator', 75000.00, 5),
+ ('Leads&Adds Facilitator', 65000.00, 5),
+ ('Facilities Manager', 65000.00, 6),
+ ('Santitation Technician', 35000.00, 6),
+ ('Contracted Cleaner', 85000.00, 6);
 
 
 
 
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', )
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('John', 'Doe', 1),
+('Bo', 'Doe', 1);
+/* ('Sue', 'Doe', 3, 1); */

e433e702bec53d6fe2aff267b83a4bcf9ddc3a29
Daymens Code <Daymenscode@gmail.com>
2024-01-20 21:31:07 -0500

    working on connecting server.js to business sql databse

--- server.js
+++ server.js
@@ -2,3 +2,15 @@
 const express = require('express')
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
+
+require('console.table');
+
+//for setting up connection to the business_db
+const db = mysql.createConnection(
+    {
+
+
+
+    },
+    console.log(`Connected to business_db`)
+);
